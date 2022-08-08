a400a538a239c6582b28fbf8fdfc3da79cfb5994
niranjannlc <niranjannlc10@gmail.com>
2022-08-08 12:51:22 +0545

    modelling the domain object in detail

--- app/src/main/java/org/lniranjan/chatclone/domain/modal/Message.kt
+++ app/src/main/java/org/lniranjan/chatclone/domain/modal/Message.kt
@@ -1,3 +1,8 @@
 package org.lniranjan.chatclone.domain.modal
 
-data class Message(val id : String)
+data class Message( val emoji: String?= null,
+                        val text: String?=null,
+                        val senderId: String?=null,
+                        val receiverId: String?=null,
+                        val timeStamp: Long=0
+)

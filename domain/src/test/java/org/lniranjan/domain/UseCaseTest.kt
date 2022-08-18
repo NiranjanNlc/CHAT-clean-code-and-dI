07aea84a75e9df70ed1a22e8c8007db03893421f
niranjannlc <niranjannlc10@gmail.com>
2022-08-18 13:35:16 +0545

    setting up usecase for the testing by before annotation

--- domain/src/test/java/org/lniranjan/domain/UseCaseTest.kt
+++ domain/src/test/java/org/lniranjan/domain/UseCaseTest.kt
@@ -1,10 +1,13 @@
 package org.lniranjan.domain
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.lniranjan.domain.usecases.UseCase
 import org.mockito.kotlin.mock
 
@@ -14,6 +17,21 @@ class UseCaseTest {
     private val configuration = UseCase.Configuration(TestCoroutineDispatcher())
     private val request = mock<UseCase.Request>()
     private val response = mock<UseCase.Response>()
+    @ExperimentalCoroutinesApi
+    private lateinit var useCase: UseCase<UseCase.Request, UseCase.Response>
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun setUp() {
+        useCase = object : UseCase<UseCase.Request, UseCase.Response>(configuration) {
+            override fun process(request: Request): Flow<Response> {
+                assertEquals(this@UseCaseTest.request, request)
+                return flowOf(response)
+            }
+        }
+    }
+
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)

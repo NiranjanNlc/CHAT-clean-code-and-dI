303ad2a077b9f25ef71d84e341a1f2f73792b726
niranjannlc <niranjannlc10@gmail.com>
2022-08-18 12:52:29 +0545

    abstracting the use cases in the domain layer

---
+++ domain/src/main/java/org/lniranjan/domain/usecases/UseCase.kt
@@ -0,0 +1,23 @@
+package org.lniranjan.domain.usecases
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import org.lniranjan.domain.entity.UseCaseException
+
+abstract class UseCase<I: UseCase.Request, O: UseCase.Response>(private val configuration: Configuration) {
+    fun execute(request: I) = process(request)
+        .map {
+            Result.success(it) as Result<O>
+        }
+        .flowOn(configuration.dispatcher)
+        .catch {
+            emit(Result.failure(UseCaseException.createFromThrowable(it)))
+        }
+    internal abstract fun process(request:I): Flow<O>
+    class Configuration(val dispatcher: CoroutineDispatcher)
+    interface Request
+    interface Response
+}
---
+++ domain/src/test/java/org/lniranjan/domain/UseCaseTest.kt
@@ -0,0 +1,19 @@
+package org.lniranjan.domain
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.lniranjan.domain.usecases.UseCase
+
+class UseCaseTest {
+
+    @ExperimentalCoroutinesApi
+    private val configuration = UseCase.Configuration(TestCoroutineDispatcher())
+    private val request = mock<UseCase.Request>()
+    private val response = mock<UseCase.Response>()
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}

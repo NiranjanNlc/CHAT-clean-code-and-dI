7550ebb752bcc8317cf29723c1949f54d45940af
niranjannlc <niranjannlc10@gmail.com>
2022-08-18 13:40:56 +0545

    executing for success tested

--- domain/src/test/java/org/lniranjan/domain/usecases/auth/SignOutTest.kt
+++ domain/src/test/java/org/lniranjan/domain/usecases/auth/SignOutTest.kt
@@ -1,14 +1,6 @@
 package org.lniranjan.domain.usecases.auth
 
-import org.junit.jupiter.api.Assertions.*
 
 internal class SignOutTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    fun setUp() {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    fun tearDown() {
-    }
 }

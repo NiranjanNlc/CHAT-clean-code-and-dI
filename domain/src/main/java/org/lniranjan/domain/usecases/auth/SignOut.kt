1143148b94cb7e7caf9635064968c8d94e5f91b2
niranjannlc <niranjannlc10@gmail.com>
2022-08-19 17:04:05 +0545

    completing auth layer of domain layer

--- domain/src/main/java/org/lniranjan/domain/usecases/auth/SignOut.kt
+++ domain/src/main/java/org/lniranjan/domain/usecases/auth/SignOut.kt
@@ -1,4 +1,20 @@
 package org.lniranjan.domain.usecases.auth
 
-class SignOut {
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.lniranjan.domain.entity.User
+import org.lniranjan.domain.repo.Authenciation
+import org.lniranjan.domain.usecases.UseCase
+
+class SignOut (
+    configuration: Configuration,
+    private val authenciation: Authenciation
+) : UseCase<SignOut.Request, SignOut.Response>(configuration) {
+    data class Request(val user: User) : UseCase.Request
+    data class Response(val credentialMatched: Boolean) : UseCase.Response
+    override fun process(request: Request): Flow<Response> = authenciation.logout(request.user)
+        .map {
+            Response(it)
+        }
+
 }
--- domain/src/main/java/org/lniranjan/domain/usecases/auth/SignUp.kt
+++ domain/src/main/java/org/lniranjan/domain/usecases/auth/SignUp.kt
@@ -1,4 +1,21 @@
 package org.lniranjan.domain.usecases.auth
 
-class SignUp {
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.lniranjan.domain.entity.User
+import org.lniranjan.domain.repo.Authenciation
+import org.lniranjan.domain.usecases.UseCase
+
+class SignUp  (
+    configuration: Configuration,
+    private val authenciation: Authenciation
+) : UseCase<SignUp.Request, SignUp.Response>(configuration) {
+    data class Request(val user: User) : UseCase.Request
+    data class Response(val credentialMatched: Boolean) : UseCase.Response
+
+    override fun process(request: Request): Flow<Response> = authenciation.sighnUp(request.user)
+        .map {
+            Response(it)
+        }
+
 }

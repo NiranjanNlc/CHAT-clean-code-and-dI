2611b2a1ebe20db63634decad304c546a95f7d4e
niranjannlc <niranjannlc10@gmail.com>
2022-08-19 16:58:28 +0545

    profile domain desighned

--- domain/src/main/java/org/lniranjan/domain/usecases/profile/GetProfileDetail.kt
+++ domain/src/main/java/org/lniranjan/domain/usecases/profile/GetProfileDetail.kt
@@ -1,4 +1,23 @@
 package org.lniranjan.domain.usecases.profile
 
-class GetProfileDetail {
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.lniranjan.domain.entity.Chat
+import org.lniranjan.domain.entity.ProfileDetail
+import org.lniranjan.domain.entity.User
+import org.lniranjan.domain.repo.Chatting
+import org.lniranjan.domain.repo.Profile
+import org.lniranjan.domain.usecases.UseCase
+import org.lniranjan.domain.usecases.chat.GetListofChat
+
+class GetProfileDetail(
+    configuration: Configuration,
+    private val  profile:Profile
+) : UseCase<GetProfileDetail.Request, GetProfileDetail.Response>(configuration) {
+    data class Request(val user: User) : UseCase.Request
+    data class Response(val profile: ProfileDetail) : UseCase.Response
+    override fun process(request: Request): Flow<Response> =  profile.getProfile(request.user)
+        .map {
+            Response(it)
+        }
 }
--- domain/src/main/java/org/lniranjan/domain/usecases/profile/UpdateProfilePhoto.kt
+++ domain/src/main/java/org/lniranjan/domain/usecases/profile/UpdateProfilePhoto.kt
@@ -1,5 +1,20 @@
 package org.lniranjan.domain.usecases.profile
 
-class UpdateProfilePhoto {
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.lniranjan.domain.entity.ProfileDetail
+import org.lniranjan.domain.repo.Profile
+import org.lniranjan.domain.usecases.UseCase
 
-}
+class UpdateProfilePhoto (
+    configuration: Configuration,
+    private val  profile: Profile
+    ) : UseCase<UpdateProfilePhoto .Request, UpdateProfilePhoto .Response>(configuration) {
+        data class Request(val updatedProfile: ProfileDetail) : UseCase.Request
+        data class Response(val updateStatus: Boolean) : UseCase.Response
+        override fun process(request: Request): Flow<Response> =
+            profile.updateProfile(request.updatedProfile)
+                .map {
+                    Response(it)
+                }
+    }

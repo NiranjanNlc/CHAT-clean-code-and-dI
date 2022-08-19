dd6eec18d3949064a8d8b1885263369942d4bb3b
niranjannlc <niranjannlc10@gmail.com>
2022-08-19 16:58:28 +0545

    profile domain desighned

--- domain/src/main/java/org/lniranjan/domain/repo/Profile.kt
+++ domain/src/main/java/org/lniranjan/domain/repo/Profile.kt
@@ -1,8 +1,10 @@
 package org.lniranjan.domain.repo
 
 import kotlinx.coroutines.flow.Flow
+import org.lniranjan.domain.entity.ProfileDetail
+import org.lniranjan.domain.entity.User
 
 interface Profile {
-    fun updateProfile(): Flow<Profile>
-    fun getProfile():Flow<Profile>
+    fun updateProfile(profileDetail: ProfileDetail):Flow<Boolean>
+    fun getProfile(user: User):Flow<ProfileDetail>
 }
--- domain/src/main/java/org/lniranjan/domain/usecases/profile/UpdateProfileDetail.kt
+++ domain/src/main/java/org/lniranjan/domain/usecases/profile/UpdateProfileDetail.kt
@@ -1,5 +1,22 @@
 package org.lniranjan.domain.usecases.profile
 
-class UpdateProfileDetail {
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.lniranjan.domain.entity.ProfileDetail
+import org.lniranjan.domain.entity.User
+import org.lniranjan.domain.repo.Profile
+import org.lniranjan.domain.usecases.UseCase
 
+class UpdateProfileDetail (
+    configuration: Configuration,
+    private val  profile: Profile
+) : UseCase<UpdateProfileDetail.Request, UpdateProfileDetail.Response>(configuration) {
+    data class Request(val updatedProfile: ProfileDetail) : UseCase.Request
+    data class Response(val updateStatus: Boolean) : UseCase.Response
+
+    override fun process(request: Request): Flow<Response> =
+        profile.updateProfile(request.updatedProfile)
+            .map {
+                Response(it)
+            }
 }

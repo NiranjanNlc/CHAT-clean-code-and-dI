7a14b74c480063969db683b4520c9ae18f4294d2
niranjannlc <niranjannlc10@gmail.com>
2022-08-22 13:18:16 +0545

    added navigation dependancy

--- app/src/main/java/org/lniranjan/chatclone/ui/activity/Auth.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/activity/Auth.kt
@@ -1,12 +1,12 @@
-package org.lniranjan.chatclone.ui
+package org.lniranjan.chatclone.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.lniranjan.chatclone.R
 
-class MainActivity : AppCompatActivity() {
+class Auth : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
     }
 }

854f3d6875343da9fef38e37fa1e2ef987648f99
niranjannlc <niranjannlc10@gmail.com>
2022-08-31 18:03:43 -0800

    addded the data binding libary

--- app/src/main/java/org/lniranjan/chatclone/ui/fragment/LoginFragment.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/fragment/LoginFragment.kt
@@ -9,18 +9,13 @@ import org.lniranjan.chatclone.R
 
 
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }

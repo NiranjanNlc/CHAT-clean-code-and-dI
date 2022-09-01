4b1f1a631f6a9957301766ea85639f51d35f0a3d
niranjannlc <niranjannlc10@gmail.com>
2022-08-31 18:03:52 -0800

    addded the data binding libary

--- app/src/main/java/org/lniranjan/chatclone/ui/activity/MainActivity.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/activity/MainActivity.kt
@@ -2,11 +2,14 @@ package org.lniranjan.chatclone.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import org.lniranjan.chatclone.R
+import org.lniranjan.chatclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }

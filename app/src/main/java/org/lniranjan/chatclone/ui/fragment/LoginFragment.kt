996502b7e274a80d9f57e02ac1698ed0374edc49
niranjannlc <niranjannlc10@gmail.com>
2022-09-02 12:55:23 +0545

    oggl between register and login completed

--- app/src/main/java/org/lniranjan/chatclone/ui/fragment/LoginFragment.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/fragment/LoginFragment.kt
@@ -5,19 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import org.lniranjan.chatclone.R
+import org.lniranjan.chatclone.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+    private lateinit var bindind : FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        bindind = DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
+        bindind.fragment = this
+        return bindind.root
     }
 
+    public fun enableLogin() {
+
+        bindind.logIn.apply {
+            background =resources.getDrawable(R.drawable.switch_trcks, null)
+            setTextColor(resources.getColor(R.color.textColor, null))
+        }
+        bindind.signUp.apply {
+            background =null
+            setTextColor(resources.getColor(R.color.pinkColor, null))
+        }
+        bindind.logInLayout.visibility = View.VISIBLE
+        bindind.signUpLayout.visibility= View.GONE
+    }
+    public fun enableSignUp() {
+        bindind.signUp.apply {
+            background =resources.getDrawable(R.drawable.switch_trcks, null)
+            setTextColor(resources.getColor(R.color.textColor, null))
+        }
+        bindind.logIn.apply {
+            background =null
+            setTextColor(resources.getColor(R.color.pinkColor, null))
+        }
+        bindind.signUpLayout.visibility = View.VISIBLE
+        bindind.logInLayout.visibility= View.GONE
+    }
  }

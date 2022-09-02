b158c47650296056647c152faee0dfb0ea923250
NLC <niranjannlc10@gmail.com>
2022-09-02 13:57:20 +0545

    UI (#20)
    * login sighnup completed
    
    * oggl between register and login completed
    
    * connecting view and viewmodel

--- app/src/main/java/org/lniranjan/chatclone/ui/fragment/LoginFragment.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/fragment/LoginFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.ViewGroup 
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import org.lniranjan.chatclone.R
 import org.lniranjan.chatclone.databinding.FragmentLoginBinding
+import org.lniranjan.chatclone.ui.viewmodel.AuthViewModel
+import org.lniranjan.chatclone.utils.toast
+import org.lniranjan.domain.entity.User
+ 
 
 
 class LoginFragment : Fragment() {
 
-    private lateinit var bindind : FragmentLoginBinding
+    private lateinit var bindind : FragmentLoginBinding 
+    private val viewModel by viewModels<AuthViewModel>()
+ 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,5 +54,24 @@ class LoginFragment : Fragment() {
         }
         bindind.signUpLayout.visibility = View.VISIBLE
         bindind.logInLayout.visibility= View.GONE
-    }
+    } 
+    public fun submit ()
+    {
+        val email =bindind.eMail.text.toString()
+        val passw =bindind.passwords.text.toString()
+        val emails =bindind.eMails.text.toString()
+        val passw1 = bindind.passwordss.text.toString()
+        val passw2 = bindind.passwords01.text.toString()
+        if(bindind.logInLayout.visibility==View.VISIBLE)
+        {
+            viewModel.login(User(mail = email, password = passw))
+        }
+        else
+        {
+          if(passw1.equals(passw2))
+              viewModel.register(User(mail = emails, password = passw1))
+            else
+                toast { "Password do not match " }
+         }
+    } 
  }
--- app/src/main/java/org/lniranjan/chatclone/ui/viewmodel/AuthViewModel.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/viewmodel/AuthViewModel.kt
@@ -1,24 +1,23 @@
 package org.lniranjan.chatclone.ui.viewmodel
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import javax.inject.Inject
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.lniranjan.domain.entity.User
+ 
 
 
 class AuthViewModel  : ViewModel() {
 
-
-    fun login(email: String, password: String) {
+ 
+     public val user: LiveData<User> = MutableLiveData<User>()
+    fun login(user: User) {
 
     }
 
-    fun register(email: String, password: String) {
+    fun register(user: User) {
+ 
 
     }
 

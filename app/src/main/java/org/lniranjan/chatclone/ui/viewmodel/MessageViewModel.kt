b43d12f68b4397add6da435d9fc3694e69afd873
niranjannlc <niranjannlc10@gmail.com>
2022-09-03 11:26:48 +0545

    connecting message viewmodel with usecase

--- app/src/main/java/org/lniranjan/chatclone/ui/viewmodel/MessageViewModel.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/viewmodel/MessageViewModel.kt
@@ -1,4 +1,38 @@
 package org.lniranjan.chatclone.ui.viewmodel
 
-class MessageViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.lniranjan.domain.entity.Message
+import org.lniranjan.domain.usecases.chat.GetListofMessage
+import org.lniranjan.domain.usecases.chat.SendMessage
+import org.lniranjan.domain.usecases.chat.UpdateMessage
+import javax.inject.Inject
+
+class MessageViewModel @Inject constructor(
+    private val sendMessage: SendMessage,
+    private val getListofMessage: GetListofMessage,
+    private val updateMessage: UpdateMessage
+):ViewModel()
+{
+    fun sendMessage(message: Message)
+    {
+        viewModelScope.launch {
+
+        }
+    }
+    fun updateMessage(message: Message)
+    {
+        viewModelScope.launch {
+
+        }
+    }
+    fun getListOfMessage(message: Message)
+    {
+        viewModelScope.launch {
+
+        }
+    }
+
+
 }

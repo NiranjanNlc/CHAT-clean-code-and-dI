cb670e569e7802cae14d2fca05e29b4704cc25d4
niranjannlc <niranjannlc10@gmail.com>
2022-09-04 17:23:35 +0545

    added the ui state

---
+++ app/src/main/java/org/lniranjan/chatclone/utils/UiState.kt
@@ -0,0 +1,7 @@
+package org.lniranjan.chatclone.utils
+
+sealed class UiState<out T : Any> {
+    object Loading : UiState<Nothing>()
+    data class Error(val errorMessage: String) : UiState<Nothing>()
+    data class Success<T : Any>(val data: T) : UiState<T>()
+}

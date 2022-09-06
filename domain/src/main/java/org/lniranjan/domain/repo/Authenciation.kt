719894e97bb97fcfacacedcbbd45404bf2a9602d
niranjannlc <niranjannlc10@gmail.com>
2022-09-06 12:44:28 +0545

    added the authenciation in the data layer

--- domain/src/main/java/org/lniranjan/domain/repo/Authenciation.kt
+++ domain/src/main/java/org/lniranjan/domain/repo/Authenciation.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 import org.lniranjan.domain.entity.User
 
 interface Authenciation {
-    fun login(email: String?, passwrd: String): Flow<Boolean>
+    suspend fun login(email: String?, passwrd: String): Flow<Boolean>
     fun logout(user: User):Flow<Boolean>
     fun sighnUp(user: User):Flow<Boolean>
 }

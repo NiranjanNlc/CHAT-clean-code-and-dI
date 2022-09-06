f36e00ce319aa56c85ff5ea5ad8f9c2a8424aab8
niranjannlc <niranjannlc10@gmail.com>
2022-09-06 12:44:28 +0545

    added the authenciation in the data layer

--- data/src/main/java/org/lniranjan/data/source/firebase/FirebaseAuthenciation.kt
+++ data/src/main/java/org/lniranjan/data/source/firebase/FirebaseAuthenciation.kt
@@ -1,19 +1,43 @@
 package org.lniranjan.data.source.firebase
 
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
 import org.lniranjan.domain.entity.User
 import org.lniranjan.domain.repo.Authenciation
+import javax.inject.Inject
 
-class FirebaseAuthenciation  : Authenciation {
-    override fun login(email: String?, passwrd: String): Flow<Boolean> {
-        TODO("Not yet implemented")
+class FirebaseAuthenciation @Inject constructor(
+    private val firebaseAuth: FirebaseAuth
+) {
+    suspend fun login(email: String?, password: String): AuthResult? {
+        return try {
+            val result = firebaseAuth.signInWithEmailAndPassword(email!!,password).await()
+            result
+        } catch (e :Exception){
+            e.printStackTrace()
+            null
+        }
     }
 
-    override fun logout(user: User): Flow<Boolean> {
-        TODO("Not yet implemented")
+    fun logout(user: User): Flow<Boolean> {
+        return flow {
+            firebaseAuth.signOut()
+            emit(true)
+        }
     }
 
-    override fun sighnUp(user: User): Flow<Boolean> {
-        TODO("Not yet implemented")
+     suspend fun sighnUp(user: User): AuthResult?{
+         return try {
+             val result = firebaseAuth.createUserWithEmailAndPassword(user.mail,user.password)
+                 .await()
+             result
+         } catch (e :Exception){
+                e.printStackTrace()
+             null
+         }
     }
 }

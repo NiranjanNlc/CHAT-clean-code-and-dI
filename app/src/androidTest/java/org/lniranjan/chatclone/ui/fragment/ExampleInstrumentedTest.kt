542f1839b0605e91b8b449f6f9bc56dcd8ae9c26
niranjannlc <niranjannlc10@gmail.com>
2022-09-14 17:33:05 +1000

    prepared the test case for registering

--- app/src/androidTest/java/org/lniranjan/chatclone/ui/fragment/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/lniranjan/chatclone/ui/fragment/ExampleInstrumentedTest.kt
@@ -1,24 +1,27 @@
-package org.lniranjan.chatclone
+package org.lniranjan.chatclone.ui.fragment
 
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.lniranjan.chatclone.R
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("org.lniranjan.chatclone", appContext.packageName)
+
+        assertEquals(R.id.eMail, R.id.eMail)
     }
 }

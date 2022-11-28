f75c9d458b8dd5138ea05afb8e68493de4b3523d
niranjannlc <niranjannlc10@gmail.com>
2022-11-28 18:06:44 +1100

    test eady authnciation

---
+++ data/src/test/java/org/lniranjan/data/repo/AuthenticationImplTest.kt
@@ -0,0 +1,28 @@
+package org.lniranjan.data.repo
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class AuthenticationImplTest {
+
+    @Test
+    fun testlogin() {
+
+    }
+
+    @Test
+    fun testlogout() {
+
+    }
+
+    @Test
+    fun testsighnUp() {
+
+    }
+
+    @Test
+    fun getUser() {
+
+    }
+}

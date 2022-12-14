1f3ac29e01e1fb6608fc10f3cdb1c6794c1c7b1f
niranjannlc <niranjannlc10@gmail.com>
2022-12-14 21:28:26 +1100

    action bar title change

--- app/src/main/java/org/lniranjan/chatclone/ui/activity/MainActivity.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package org.lniranjan.chatclone.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.lniranjan.chatclone.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/org/lniranjan/chatclone/ui/activity/SettingActivity.kt
+++ app/src/main/java/org/lniranjan/chatclone/ui/activity/SettingActivity.kt
@@ -2,10 +2,16 @@ package org.lniranjan.chatclone.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.lniranjan.chatclone.R
+
+/* activity to displau and update the user info */
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        //  change the  title of the action bar
+        supportActionBar?.title = " Update  the profile info"
+
     }
 }

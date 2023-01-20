3ae4020dc9d29720a3cca8032a65f5f37f93e522
niranjannlc <77341018@nou.edu.np>
2023-01-20 10:34:55 +0545

    remanemed to authsute

--- data/src/test/java/org/lniranjan/data/AuthTestSute.kt
+++ data/src/test/java/org/lniranjan/data/AuthTestSute.kt
@@ -3,12 +3,11 @@ package org.lniranjan.data
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 import org.lniranjan.data.repo.AuthenticationImplTest
-import org.lniranjan.data.source.firebase.FirebaseAuthenciation
 import org.lniranjan.data.source.firebase.FirebaseAuthenciationTest
 
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(FirebaseAuthenciationTest::class, AuthenticationImplTest::class)
-class TestSute {
+class AuthTestSute {
 
 }

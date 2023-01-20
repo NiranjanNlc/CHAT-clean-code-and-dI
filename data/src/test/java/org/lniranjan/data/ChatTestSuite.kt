b3d081b52cf1f9f85bc2f2737e530a36f128f0da
niranjannlc <77341018@nou.edu.np>
2023-01-20 14:30:10 +0545

    renamed to chattestsuite

---
+++ data/src/test/java/org/lniranjan/data/ChatTestSuite.kt
@@ -0,0 +1,11 @@
+package org.lniranjan.data
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.lniranjan.data.source.firebase.FireBaseChatsTest
+
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(FireBaseChatsTest::class)
+class ChatTestSuite {
+}
--- data/src/test/java/org/lniranjan/data/TestSuite.kt
+++
@@ -1,4 +0,0 @@
-package org.lniranjan.data
-
-class TestSuite {
-}
